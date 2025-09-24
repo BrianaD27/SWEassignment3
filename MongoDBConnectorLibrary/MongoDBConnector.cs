5fba808d7290a2f66cbf07f949e277424bdb530c
Briana Davis <brianad570@gmail.com>
2025-09-24 01:05:56 -0400

    Initialized MongoDB Database

--- MongoDBConnectorLibrary/MongoDBConnector.cs
+++ MongoDBConnectorLibrary/MongoDBConnector.cs
@@ -1,6 +1,15 @@
 ﻿namespace MongoDBConnectorLibrary;
+using MongoDB.Driver;
 
 public class MongoDBConnector
 {
+    // Field to hold the MongoDB CLient Instance 
+    private readonly IMongoDatabase _database;
 
+    // Constructor takes a connection string
+    public MongoDBConnector(string connectionString)
+    {
+        // Create a MongoClient using the connection string
+        var client = new MongoClient(connectionString);
+    }
 }
