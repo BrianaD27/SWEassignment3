c30825766ddefe8aff995bd0f4124bf919fdaa8e
Briana Davis <brianad570@gmail.com>
2025-09-24 16:54:00 -0400

    test: Added MongoDB Success Test Case

--- TestMongoDBConnector/MongoDBConnector.Test.cs
+++ TestMongoDBConnector/MongoDBConnector.Test.cs
@@ -1,10 +1,30 @@
 ﻿namespace TestMongoDBConnector;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+using System.Threading.Tasks;
+using MongoDBConnectorLibrary;
+using Testcontainers.MongoDb;
 
+public class UnitTest1 : IAsyncLifetime
+{
+    private String _connectionString = string.Empty;
+    private readonly MongoDbContainer _mongo = new MongoDbBuilder().WithImage("mongo:7").Build();
+
+    public async Task InitializeAsync()
+    {
+        await _mongo.StartAsync();
+        _connectionString = _mongo.GetConnectionString();
     }
+
+    public async Task DisposeAsync()
+    {
+        await _mongo.DisposeAsync();
+    }
+
+    [Fact]
+    public void MongoDB_Ping_Returns_True()
+    {
+        var connector = new MongoDBConnector(_connectionString);
+        Assert.True(connector.MongoDBPing());
+    }
+
 }
